f094f62298657463005fdf7d4ecce5d1a10304f3
KanungoIshan <ikanungo17@gmail.com>
2020-09-25 11:20:00 +0530

    updated dependencies

--- controllers/getUsers.js
+++ controllers/getUsers.js
@@ -6,7 +6,7 @@ exports.fetchUsers = (req,res,next) =>{
     dao.fetchUsers().then((result) => {
         const Users = new Array();
         result.records.forEach((record) => {
-            Users.push(new User(record._fields[0].properties.name,record._fields[0].properties.password, record._fields[0].properties.emailId, record._fields[0].properties.username));
+            Users.push(new User(record._fields[0].properties.name, record._fields[0].properties.password, record._fields[0].properties.emailId, record._fields[0].properties.username));
         });
 
         res.status(200).json({Users});
